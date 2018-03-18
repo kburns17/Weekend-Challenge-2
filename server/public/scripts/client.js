e4dd80fb7b104a033f59d7dc7e86a57361493e30
Kerry Burns <kerryalanburns@gmail.com>
2018-03-18 13:36:20 -0500

    most functions in

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,9 @@ function readyNow() {
     $('#subtract').on('click', runEquation)
     $('#multiply').on('click', runEquation)
     $('#divide').on('click', runEquation)
-
+    getAllEquations();
 }
+
 function runEquation() {
     console.log('in runEquation');
     let num1 = $('#num1').val();
@@ -22,6 +23,24 @@ function runEquation() {
     }).fail(function(response){
         alert('Something went wrong...')
     })
+}//end runEquation
 
-
+function getAllEquations() {
+    $.ajax({
+        type: 'GET',
+        url: '/equation',
+    }).done(function(response){
+        //the reponse is our gameCollection array
+    }) 
 }
+function appendToDom(equationCollection) {
+    $('.container').empty();
+    for (const equation of equationCollection) {
+        console.log('equation', equation);
+        let tr = $('<tr></tr>')
+        tr.append('<td>' + equation.firstNumber + '</td>')
+        tr.append('<td>' + equation.secondNumber + '</td>')
+        $('.container').append(tr);
+    }
+}
+
--- server/server.js
+++ server/server.js
@@ -1,18 +1,32 @@
 let express = require('express');
 let app = express();
 const PORT = 5000;
-
 app.use(express.static('server/public'));
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 
+let equationCollection = [];
 
+//adding equation
 app.post('/equation', (req, res) => {
     console.log(req.body);
     let equationToAdd = req.body;
+    let num1 = equationToAdd.firstNumber;
+    let num2 = equationToAdd.secondNumber;
+    equationCollection.push(equationToAdd);
+    console.log('collection', equationCollection);
     res.sendStatus(200);
 });
 
+// getting equation
+app.get('/equation', (req, res) => { 
+    res.send(equationCollection);
+});
+
+function doMath() {
+    
+}
+
 app.listen(5000, () => {
     console.log('app is running on port 5000');
-    });
+});
